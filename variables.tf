e511975b8bde949ff18fdab5df4039d2b8c37eda
anirudhnikhils <78731944+anirudhnikhils@users.noreply.github.com>
2024-08-24 11:59:10 +0530

    Add files via upload
    terraform project

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "cidr" {
+    default = "10.0.0.0/16"
+  
+}
+variable "cidr_subnet1" {
+    default = "10.0.0.0/24"
+  
+}
+variable "zone1" {
+    default = "us-east-1a"
+  
+}
+variable "cidr_subnet2" {
+    default = "10.0.1.0/24"
+  
+}
+variable "zone2" {
+    default = "us-east-1b"
+  
+}
+variable "instancetype" {
+    default = "t2.micro"
+  
+}
